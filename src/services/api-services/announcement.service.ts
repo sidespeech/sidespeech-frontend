3c89abc90a5907d08e48bc39122a7c4b893f721f
tomasscattini <tomasscattini@gmail.com>
2023-01-02 12:18:47 -0500

    Feature: add endpoint for admin announcements

--- src/services/api-services/announcement.service.ts
+++ src/services/api-services/announcement.service.ts
@@ -1,40 +1,53 @@
-import { BASE_URL } from "../../constants/constants";
-import { Announcement } from "../../models/Announcement";
-import { BaseApiService } from "./base-api.service";
+import { BASE_URL } from '../../constants/constants';
+import { Announcement } from '../../models/Announcement';
+import { BaseApiService } from './base-api.service';
 
 // Create an API Service class
 let instance: AnnouncementService;
 class AnnouncementService extends BaseApiService {
+	static getInstance() {
+		if (!instance) instance = new AnnouncementService();
+		return instance;
+	}
 
-  static getInstance() {
-    if (!instance) instance = new AnnouncementService();
-    return instance;
-  }
+	// This method will create an announcement call to the API
+	async createAnnouncement(announcement: any, creatorAddress: any, channelId: string): Promise<Announcement> {
+		const createAnnouncement = await this.post(`${BASE_URL}/announcement`)
+			.send({
+				content: announcement,
+				creatorAddress: creatorAddress,
+				channelId,
+				timestamp: Date.now().toString()
+			})
+			.set('accept', 'json');
 
-    // This method will create an announcement call to the API
-    async createAnnouncement(
-        announcement: any,
-        creatorAddress: any,
-        channelId: string
-      ): Promise<Announcement> {
-        const createAnnouncement = await this.post(`${BASE_URL}/announcement`)
-          .send({
-            content: announcement,
-            creatorAddress: creatorAddress,
-            channelId,
-            timestamp: Date.now().toString(),
-          })
-          .set("accept", "json");
-    
-        return new Announcement(createAnnouncement.body);
-      }
-    
-      // Get all the announcements
-      async getAnnouncements(): Promise<any> {
-        const res = await this.get(`${BASE_URL}/announcement`);
-        return res.body.map((a: any) => new Announcement(a));
-      }
+		return new Announcement(createAnnouncement.body);
+	}
 
+	async createAnnouncementAsAdmin(
+		announcement: any,
+		creatorAddress: any,
+		channelId: string,
+		sideId: string
+	): Promise<Announcement> {
+		const createAnnouncement = await this.post(`${BASE_URL}/announcement/admin`)
+			.send({
+				content: announcement,
+				creatorAddress: creatorAddress,
+				channelId,
+				id: sideId,
+				timestamp: Date.now().toString()
+			})
+			.set('accept', 'json');
+
+		return new Announcement(createAnnouncement.body);
+	}
+
+	// Get all the announcements
+	async getAnnouncements(): Promise<any> {
+		const res = await this.get(`${BASE_URL}/announcement`);
+		return res.body.map((a: any) => new Announcement(a));
+	}
 }
 
 export default AnnouncementService.getInstance();
